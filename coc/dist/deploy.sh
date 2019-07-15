abdb12819b7ecedbcf1d39782e253970a7712660
JTrillo <jokinator20@nuclear-1.europe-west1-b.c.hyperledger-jte.internal>
2019-07-15 08:32:17 +0000

    nuclear folder clean up

--- coc/dist/deploy.sh
+++ coc/dist/deploy.sh
@@ -5,10 +5,10 @@ composer archive create -t dir -n ../ -a archive.bna
 composer network install -a archive.bna -c PeerAdmin@hlfv1
 
 # Start the network
-composer network start -n coc -c PeerAdmin@hlfv1 -V 0.0.1 -A admin -S adminpw
+composer network start -n cocv2 -c PeerAdmin@hlfv1 -V 0.0.1 -A admin -S adminpw
 
 # Import the card generated
-composer card import -f admin@coc.card
+composer card import -f admin@cocv2.card
 
 # Test the network connection
-composer network ping -c admin@coc
+composer network ping -c admin@cocv2
