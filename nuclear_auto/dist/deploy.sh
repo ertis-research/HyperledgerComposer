ad27f7150e3f98c97de62ca58992b0d0f7e6fe2d
JTrillo <jokinator20@gmail.com>
2019-07-30 11:35:37 +0200

    version of nuclear BN w/ automatic analysis

---
+++ nuclear_auto/dist/deploy.sh
@@ -0,0 +1,14 @@
+# Create the BNA
+composer archive create -t dir -n ../ -a archive.bna
+
+# Install the BNA
+composer network install -a archive.bna -c PeerAdmin@hlfv1
+
+# Start the network
+composer network start -n nuclear_auto -c PeerAdmin@hlfv1 -V 0.0.1 -A admin -S adminpw
+
+# Import the card generated
+composer card import -f admin@nuclear_auto.card
+
+# Test the network connection
+composer network ping -c admin@nuclear_auto
